a022431644027cd2ebbdaecd2bc76e2ce0d75707
kswaminarmada <34015922+kswaminarmada@users.noreply.github.com>
2017-12-02 08:19:36 +0530

    commit

--- loginW.py
+++ loginW.py
@@ -2,19 +2,20 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import DB_manager
 from Main import Ui_MainWindow
 from MyConfig import MyConfigs
+from MainW import Ui_MainWindow
 
 msgbox = QtWidgets.QMessageBox
 
-class Ui_login(object):
+class Ui_login(QtWidgets.QDialog):
     def __init__(self):
+        super().__init__()
         cfgs = MyConfigs()
         connstring = cfgs.Get_db_config()
         self.dbCon = DB_manager.DatabaseUtility(**connstring)
     
     def Mainwinshow(self,IsAdmin=False):
-        self.MainWindow = QtWidgets.QMainWindow()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self.MainWindow, IsAdmin)
+        self.MainWindow = Ui_MainWindow()
+        self.MainWindow.setupUi(IsAdmin)
         login.hide()
         self.MainWindow.show()
         
@@ -26,7 +27,7 @@ class Ui_login(object):
             IsAdm = bool(tbl[3])
             self.Mainwinshow(IsAdm)
         else:
-            if msgbox.information(login,"Invalid Credentials","Invalid Password, try Again",msgbox.Ok|msgbox.Ignore) == msgbox.Ignore:
+            if msgbox.information(self,"Invalid Credentials","Invalid Password, try Again",msgbox.Ok|msgbox.Ignore) == msgbox.Ignore:
                 self.Mainwinshow(False)
             else:
                 self.txtPwd.setFocus() 
@@ -36,42 +37,42 @@ class Ui_login(object):
         for usr in tbl:
             self.cboUser.addItem(usr[0])
 
-    def setupUi(self, login):
-        login.setObjectName("login")
-        login.resize(317, 194)
-        login.setWindowTitle("Login")
+    def setupUi(self):
+        self.setObjectName("login")
+        self.resize(317, 194)
+        self.setWindowTitle("Login")
         self.centralwidget = QtWidgets.QWidget(login)
         self.centralwidget.setObjectName("centralwidget")
-        self.cboUser = QtWidgets.QComboBox(self.centralwidget)
+        self.cboUser = QtWidgets.QComboBox(self)
         self.cboUser.setGeometry(QtCore.QRect(100, 40, 191, 22))
         self.cboUser.setObjectName("cboUser")
-        self.txtPwd = QtWidgets.QLineEdit(self.centralwidget)
+        self.txtPwd = QtWidgets.QLineEdit(self)
         self.txtPwd.setGeometry(QtCore.QRect(100, 70, 191, 20))
         self.txtPwd.setInputMask("")
         self.txtPwd.setEchoMode(QtWidgets.QLineEdit.Password)
         self.txtPwd.setClearButtonEnabled(True)
         self.txtPwd.setObjectName("txtPwd")
-        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label = QtWidgets.QLabel(self)
         self.label.setGeometry(QtCore.QRect(40, 40, 47, 13))
         self.label.setMinimumSize(QtCore.QSize(47, 13))
         self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2 = QtWidgets.QLabel(self)
         self.label_2.setGeometry(QtCore.QRect(30, 70, 61, 20))
         self.label_2.setMinimumSize(QtCore.QSize(61, 20))
         self.label_2.setScaledContents(False)
         self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.label_2.setObjectName("label_2")
-        self.cmdLogin = QtWidgets.QPushButton(self.centralwidget)
+        self.cmdLogin = QtWidgets.QPushButton(self)
         self.cmdLogin.setGeometry(QtCore.QRect(90, 120, 75, 31))
         self.cmdLogin.setObjectName("cmdLogin")
         self.cmdLogin.clicked.connect(self.checklogin)
-        self.cmdCancel = QtWidgets.QPushButton(self.centralwidget)
+        self.cmdCancel = QtWidgets.QPushButton(self)
         self.cmdCancel.setGeometry(QtCore.QRect(170, 120, 75, 31))
         self.cmdCancel.setObjectName("cmdCancel")
-        login.setCentralWidget(self.centralwidget)
+        #login.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(login)
+        self.retranslateUi()
         self.cmdCancel.clicked.connect(login.close)
         QtCore.QMetaObject.connectSlotsByName(login)
         login.setTabOrder(self.cboUser, self.txtPwd)
@@ -79,7 +80,7 @@ class Ui_login(object):
         login.setTabOrder(self.cmdLogin, self.cmdCancel)
         self.FillUsers()
 
-    def retranslateUi(self, login):
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
         self.label.setText(_translate("login", "User :"))
         self.label_2.setText(_translate("login", "Password :"))
@@ -89,9 +90,8 @@ class Ui_login(object):
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    login = QtWidgets.QMainWindow()
-    ui = Ui_login()
-    ui.setupUi(login)
+    login = Ui_login()
+    login.setupUi()
     login.show()
     sys.exit(app.exec_())
 
